b978176aefeab8b2a348010b30b902d44c771428
unknown <lucas.vignijr@gmail.com>
2019-10-01 18:27:06 +0200

    loader

--- js/application.js
+++ js/application.js
@@ -9,9 +9,22 @@
 //   })
 
 // });
+setTimeout(showPage,2000);
+// $.ready(function(){
+//   $("#loading").fadeOut(2000);
+//   $("#preloader").fadeOut(2000);
+// });
+// $(window).load(showPage);
+// window.onload = function () {
+//   this.showPage();
+// } 
 
-
-
+function showPage() {
+  $("#loading").fadeOut(2000);
+  $("#preloader").fadeOut(1000);
+}
+  // document.getElementById("loading").style.display = "none";
+  // document.getElementById("container-page").style.display = "block";
 
 
 (function() {
@@ -39,6 +52,45 @@
           items[i].classList.add("in-view");
         }
       }
+    }
+  
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  
+  })();
+
+  (function() {
+
+    'use strict';
+  
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+    var intro = document.querySelectorAll(".about-content");
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+      
+        if (isElementInViewport(intro)) {
+          intro.classList.add("in-view");
+        }
+      
     }
   
     // listen for events
@@ -116,3 +168,6 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
--- js/typeIt.js
+++ js/typeIt.js
@@ -1,8 +1,9 @@
 new TypeIt('#subtitle', {
     speed: 50
   })
+  .pause(2500)
   .type('Bienvenue sur mon portfolio')
-  .pause(500)
+  .pause(300)
   .options({speed: 200})
   .delete(17)
   .options({speed: 50})
